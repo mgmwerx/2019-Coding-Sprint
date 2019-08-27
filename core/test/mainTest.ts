f18e7dfa848ec29786ee3f77b35d1e7acc03a4e4
Scott Escue <scott.escue@southernfiredsoftware.com>
2019-08-26 21:18:51 -0500

    Moving Core server project out of the 2019-Coding-Sprint repo and into its own top-level repo.

--- core/test/mainTest.ts
+++
@@ -1,8 +0,0 @@
-// tslint:disable: no-unused-expression
-import { expect } from "chai";
-
-describe("Tests", () => {
-    it("Should have tests", () => {
-        expect(true).to.be.true;
-    });
-});
